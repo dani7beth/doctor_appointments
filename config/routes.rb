48491955069b785f422e948cd7bebe18248b1880
dani7beth <danielledona28@gmail.com>
2020-11-22 14:38:51 -0700

    added a static home page for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # get 'static_pages/home'
+  # get 'staticpages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'doctors#index' #temp will be making root a static home page
+# get 'static_pages/home'
+root 'static_pages#home' #temp will be making root a static home page
+# get '/home', to: 'static_pages#home'
 
 resources :users
 resources :doctors do
